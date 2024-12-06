e3677380cfa3f08874d87700d8f2974459474721
RustyYato <rustyyato@gmail.com>
2024-12-05 19:09:09 -0700

    finite sigmas

--- Math/Data/Fin/Basic.lean
+++ Math/Data/Fin/Basic.lean
@@ -70,3 +70,145 @@ def Fin.pair.inj : Function.Injective₂ (Fin.pair (n := n) (m := m)) := by
   rw [pair_pair_left] at h₀
   rw [pair_pair_right] at h₁
   apply And.intro <;> (symm; assumption)
+
+def Fin.sum [Zero α] [Add α] : ∀{n}, (f: Fin n -> α) -> α
+| 0, _ => 0
+| _ + 1, f => f 0 + Fin.sum (f ∘ Fin.succ)
+
+def Fin.sum_to [Zero α] [Add α] (x: Fin n) (f: Fin n -> α) : α :=
+  Fin.sum <| fun y => if y < x then f y else 0
+
+def Fin.sum_zero :
+  Fin.sum (fun _: Fin n => 0) = (0: Nat) := by
+  induction n with
+  | zero => rfl
+  | succ n ih =>
+    unfold sum
+    rw [Nat.zero_add]
+    exact ih
+
+def Fin.sum_to_zero (f: Fin (n + 1) -> Nat) :
+  Fin.sum_to 0 f = 0 := by
+  unfold sum_to
+  simp
+  rw [sum_zero]
+
+def Fin.sum_succ (f: Fin (n + 1) -> Nat):
+  Fin.sum f = f 0 + Fin.sum (f ∘ Fin.succ) := rfl
+
+def Fin.sum_to_succ (f: Fin (n + 1) -> Nat) (x: Fin (n + 1)) (h: x ≠ 0) :
+  Fin.sum_to x f = f 0 + Fin.sum_to (x.pred h) (f ∘ Fin.succ) := by
+  unfold sum_to
+  rw [sum_succ]
+  rw[if_pos]
+  congr
+  · funext x'
+    dsimp
+    split <;> rename_i g
+    rw [if_pos]
+    apply Fin.succ_lt_succ_iff.mp
+    rw [Fin.succ_pred]
+    assumption
+    rw [if_neg]
+    intro h
+    apply g
+    apply Fin.pred_lt_pred_iff.mp
+    rw [Fin.pred_succ]
+    assumption
+    exact Fin.succ_ne_zero _
+  apply Nat.zero_lt_of_ne_zero
+  intro g
+  apply h
+  exact Fin.val_inj.mp g
+
+def Fin.le_sum (x: Fin n) (f: Fin n -> Nat) : f x ≤ sum f := by
+  induction n with
+  | zero => exact x.elim0
+  | succ n ih =>
+    if h:x = 0 then
+      subst x
+      apply Nat.le_add_right
+    else
+      apply flip Nat.le_trans
+      apply Nat.le_add_left
+      apply flip Nat.le_trans
+      apply ih (x.pred h)
+      dsimp
+      rw [Fin.succ_pred]
+      apply Nat.le_refl
+
+def Fin.sum_to_lt_sum (x: Fin n) (f: Fin n -> Nat) (y: Fin (f x)) :
+  sum_to x f + y.val < sum f := by
+  induction n with
+  | zero => exact x.elim0
+  | succ n ih =>
+    if h:x = 0 then
+      subst x
+      rw [sum_to_zero, Nat.zero_add]
+      apply Nat.lt_of_lt_of_le
+      apply Fin.isLt
+      apply le_sum
+    else
+      replace ih := ih (x.pred h) (f ∘ Fin.succ)
+      simp at ih
+      rw [Fin.succ_pred] at ih
+      replace ih := ih y
+      unfold sum_to at ih
+      simp at ih
+      rw [Fin.sum_to_succ, sum_succ]
+      rw [Nat.add_assoc]
+      apply Nat.add_lt_add_left
+      apply ih
+
+def Fin.lt_add_sum_to_inj (f: Fin n -> Nat)
+    (x₀ x₁: Fin n)
+    (y₀: Fin (f x₀))
+    (y₁: Fin (f x₁)) :
+  y₀.val + sum_to x₀ f = y₁.val + sum_to x₁ f -> x₀ = x₁ ∧ HEq y₀ y₁ := by
+  intro eq
+  induction n with
+  | zero => exact x₀.elim0
+  | succ n ih =>
+    if h₀:x₀ = 0 then
+      subst x₀
+      rw [sum_to_zero, Nat.add_zero] at eq
+      if h₁:x₁ = 0 then
+        subst x₁
+        rw [sum_to_zero] at eq
+        cases Fin.val_inj.mp eq
+        apply And.intro
+        rfl
+        rfl
+      else
+        rw [sum_to_succ _ _ h₁]  at eq
+        have := y₀.isLt
+        rw [eq] at this
+        rw [←Nat.add_assoc, Nat.add_comm _ (f 0), Nat.add_assoc] at this
+        have := Nat.not_lt_of_le (Nat.le_add_right _ _) this
+        contradiction
+    else
+      rw [sum_to_succ _ _ h₀]  at eq
+      if h₁:x₁ = 0 then
+        subst x₁
+        rw [sum_to_zero] at eq
+        replace eq : _ = y₁.val := eq
+        have := y₁.isLt
+        rw [←eq] at this
+        rw [←Nat.add_assoc, Nat.add_comm _ (f 0), Nat.add_assoc] at this
+        have := Nat.not_lt_of_le (Nat.le_add_right _ _) this
+        contradiction
+      else
+        rw [sum_to_succ _ _ h₁]  at eq
+        rw [Nat.add_left_comm y₀, Nat.add_left_comm y₁] at eq
+        replace eq := Nat.add_left_cancel eq
+        replace ih := ih (f ∘ Fin.succ) (x₀.pred h₀) (x₁.pred h₁) (by
+          apply Fin.mk y₀.val
+          simp) (by
+          apply Fin.mk y₁.val
+          simp) eq
+        simp at ih
+        obtain ⟨a, b⟩ := ih
+        cases a
+        cases y₀; cases y₁
+        cases b
+        apply And.intro <;> rfl
--- Math/Type/Finite.lean
+++ Math/Type/Finite.lean
@@ -5,7 +5,7 @@ import Math.Data.Fin.Basic
 class inductive IsFinite (α: Sort*): Prop where
 | intro (limit: Nat) : (α ↪ Fin limit) -> IsFinite α
 
-def Type.IsFinite.existsIso (α: Sort*) [h: IsFinite α] : ∃card, _root_.Nonempty (α ≃ Fin card) := by
+def existsEquiv (α: Sort*) [h: IsFinite α] : ∃card, _root_.Nonempty (α ≃ Fin card) := by
   obtain ⟨ limit, emb ⟩ := h
   induction limit with
   | zero =>
@@ -78,6 +78,13 @@ def Type.IsFinite.existsIso (α: Sort*) [h: IsFinite α] : ∃card, _root_.Nonem
           apply lt_of_not_le
           assumption
         · exact emb.inj (Fin.pred_inj.mp eq)
+
+noncomputable
+def IsFinite.card α [IsFinite α] : Nat :=
+  Classical.choose (existsEquiv α)
+noncomputable
+def IsFinite.toEquiv α [IsFinite α] : α ≃ Fin (card α) :=
+  Classical.choice (Classical.choose_spec (existsEquiv α))
 
 instance IsFinite.ofFin : IsFinite (Fin n) := by
   apply IsFinite.intro n
@@ -178,3 +185,123 @@ instance IsFinite.ofPProd {α β: Sort*} [ha: IsFinite α] [hb: IsFinite β] : I
     congr
     exact aemb.inj aeq
     exact bemb.inj beq
+
+instance IsFinite.ofSigma {α: Type*} {β: α -> Type*} [ha: IsFinite α] [hb: ∀x, IsFinite (β x)] : IsFinite ((x: α) × β x) := by
+  have equiv := toEquiv α
+  have βequiv : {x: α} -> β x ≃ Fin (card (β x)) := fun {x} => toEquiv _
+  apply IsFinite.intro <| Fin.sum fun x => card (β (equiv.invFun x))
+  apply Embedding.mk
+  case toFun =>
+    intro ⟨a, b⟩
+    apply Fin.mk _ _
+    apply _ + Fin.sum_to (equiv a) fun x => card (β (equiv.invFun x))
+    exact (βequiv b).val
+    simp
+    rw [←Nat.sub_add_cancel (n := Fin.sum _) (m := (βequiv b).val), Nat.add_comm]
+    apply Nat.add_lt_add_right
+    show _ < _ - (βequiv.toFun b).val
+    apply Nat.lt_sub_of_add_lt
+    have := Fin.sum_to_lt_sum (x := equiv a) (f := fun x => card (β (equiv.invFun x))) (y := by
+      dsimp
+      show Fin (card (β (equiv.invFun (equiv.toFun _))))
+      apply Fin.mk
+      rw [equiv.leftInv]
+      exact (βequiv.toFun b).isLt)
+    dsimp at this
+    apply this
+    apply Nat.le_trans
+    apply Nat.le_of_lt
+    apply Fin.isLt
+    have : card (β (equiv.invFun (equiv.toFun _))) ≤ _ := Fin.le_sum (equiv a) (fun x => card (β (equiv.invFun x)))
+    rw [equiv.leftInv] at this
+    assumption
+  case inj =>
+    dsimp
+    intro ⟨xa, xb⟩ ⟨ya, yb⟩ eq
+    dsimp at eq
+    replace eq := Fin.mk.inj eq
+    have ⟨xa_eq_ya, xb_eq_yb⟩ := Fin.lt_add_sum_to_inj (fun x => card (β (equiv.invFun x)))
+      (equiv xa) (equiv ya) (by
+        apply Fin.mk (βequiv xb).val
+        simp [DFunLike.coe, IsEquivLike.coe]
+        have := equiv.leftInv xa
+        apply Nat.lt_of_lt_of_le
+        apply Fin.isLt
+        apply Nat.le_of_eq
+        congr
+        symm; assumption) (by
+        apply Fin.mk (βequiv yb).val
+        simp [DFunLike.coe, IsEquivLike.coe]
+        have := equiv.leftInv ya
+        apply Nat.lt_of_lt_of_le
+        apply Fin.isLt
+        apply Nat.le_of_eq
+        congr
+        symm; assumption) eq
+    clear eq
+    cases equiv.toFun_inj xa_eq_ya
+    congr
+    simp at xb_eq_yb
+    replace xb_eq_yb := Fin.val_inj.mp xb_eq_yb
+    cases βequiv.toFun_inj xb_eq_yb
+    rfl
+
+instance IsFinite.ofPSigma {α: Type*} {β: α -> Type*} [ha: IsFinite α] [hb: ∀x, IsFinite (β x)] : IsFinite ((x: α) ×' β x) := by
+  have equiv := toEquiv α
+  have βequiv : {x: α} -> β x ≃ Fin (card (β x)) := fun {x} => toEquiv _
+  apply IsFinite.intro <| Fin.sum fun x => card (β (equiv.invFun x))
+  apply Embedding.mk
+  case toFun =>
+    intro ⟨a, b⟩
+    apply Fin.mk _ _
+    apply _ + Fin.sum_to (equiv a) fun x => card (β (equiv.invFun x))
+    exact (βequiv b).val
+    simp
+    rw [←Nat.sub_add_cancel (n := Fin.sum _) (m := (βequiv b).val), Nat.add_comm]
+    apply Nat.add_lt_add_right
+    show _ < _ - (βequiv.toFun b).val
+    apply Nat.lt_sub_of_add_lt
+    have := Fin.sum_to_lt_sum (x := equiv a) (f := fun x => card (β (equiv.invFun x))) (y := by
+      dsimp
+      show Fin (card (β (equiv.invFun (equiv.toFun _))))
+      apply Fin.mk
+      rw [equiv.leftInv]
+      exact (βequiv.toFun b).isLt)
+    dsimp at this
+    apply this
+    apply Nat.le_trans
+    apply Nat.le_of_lt
+    apply Fin.isLt
+    have : card (β (equiv.invFun (equiv.toFun _))) ≤ _ := Fin.le_sum (equiv a) (fun x => card (β (equiv.invFun x)))
+    rw [equiv.leftInv] at this
+    assumption
+  case inj =>
+    dsimp
+    intro ⟨xa, xb⟩ ⟨ya, yb⟩ eq
+    dsimp at eq
+    replace eq := Fin.mk.inj eq
+    have ⟨xa_eq_ya, xb_eq_yb⟩ := Fin.lt_add_sum_to_inj (fun x => card (β (equiv.invFun x)))
+      (equiv xa) (equiv ya) (by
+        apply Fin.mk (βequiv xb).val
+        simp [DFunLike.coe, IsEquivLike.coe]
+        have := equiv.leftInv xa
+        apply Nat.lt_of_lt_of_le
+        apply Fin.isLt
+        apply Nat.le_of_eq
+        congr
+        symm; assumption) (by
+        apply Fin.mk (βequiv yb).val
+        simp [DFunLike.coe, IsEquivLike.coe]
+        have := equiv.leftInv ya
+        apply Nat.lt_of_lt_of_le
+        apply Fin.isLt
+        apply Nat.le_of_eq
+        congr
+        symm; assumption) eq
+    clear eq
+    cases equiv.toFun_inj xa_eq_ya
+    congr
+    simp at xb_eq_yb
+    replace xb_eq_yb := Fin.val_inj.mp xb_eq_yb
+    cases βequiv.toFun_inj xb_eq_yb
+    rfl
