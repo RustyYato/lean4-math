6b2d4991ca98a68f4013d70d4077fb665bf8130d
RustyYato <rustyyato@gmail.com>
2024-12-08 19:18:28 -0700

    implement union

--- Math/Data/Zf/Basic.lean
+++ Math/Data/Zf/Basic.lean
@@ -1,5 +1,6 @@
 import Math.Data.QuotLike.Basic
 import Math.Type.Notation
+import Math.Data.Set.Basic
 
 namespace ZfSet
 
@@ -65,6 +66,19 @@ instance : QuotientLike setoid ZfSet where
 
 local notation "⟦" a "⟧" => mk a
 
+def ind : {motive: ZfSet -> Prop} -> (mk: ∀x, motive ⟦x⟧) -> ∀x, motive x := Quotient.ind
+def ind₂ : {motive: ZfSet -> ZfSet -> Prop} -> (mk: ∀x y, motive ⟦x⟧ ⟦y⟧) -> ∀x y, motive x y := Quotient.ind₂
+def ind₃ : {motive: ZfSet -> ZfSet -> ZfSet -> Prop} -> (mk: ∀x y z, motive ⟦x⟧ ⟦y⟧ ⟦z⟧) -> ∀x y z, motive x y z := by
+  intro motive h x y z
+  induction x, y using ind₂ with | mk x y =>
+  induction z using ind with | mk z =>
+  apply h
+def ind₄ : {motive: ZfSet -> ZfSet -> ZfSet -> ZfSet -> Prop} -> (mk: ∀w x y z, motive ⟦w⟧ ⟦x⟧ ⟦y⟧ ⟦z⟧) -> ∀w x y z, motive w x y z := by
+  intro motive h w x y z
+  induction w, x using ind₂ with | mk w x =>
+  induction y, z using ind₂ with | mk y z =>
+  apply h
+
 @[refl]
 def Equiv.refl : ∀(a: Pre), a ≈ a := Equiv.refl'
 @[symm]
@@ -89,8 +103,41 @@ instance : Membership ZfSet ZfSet where
     apply Equiv.trans _ prf
     assumption
 
+instance : HasSubset Pre where
+  Subset a b := ∀x ∈ a, x ∈ b
+
+instance : HasSubset ZfSet where
+  Subset a b := ∀x ∈ a, x ∈ b
+
 @[simp]
 def mk_mem (a b: Pre) : (⟦a⟧ ∈ ⟦b⟧) = (a ∈ b) := rfl
+
+def Pre.mem_def {a: Pre} : ∀{x: a.Type}, a.Mem x ∈ a := by
+  intro x
+  cases a with | intro a amem =>
+  exists x
+
+def Pre.ext (a b: Pre) : (∀x, x ∈ a ↔ x ∈ b) -> a ≈ b := by
+  intro h
+  cases a with | intro a amem =>
+  cases b with | intro b bmem =>
+  apply And.intro
+  intro x
+  have ⟨y, prf⟩  := (h (amem x)).mp Pre.mem_def
+  exists y
+  intro x
+  have ⟨y, prf⟩  := (h (bmem x)).mpr Pre.mem_def
+  exists y
+  symm; assumption
+
+@[ext]
+def ext (a b: ZfSet) : (∀x, x ∈ a ↔ x ∈ b) -> a = b := by
+  intro h
+  induction a using ind with | mk a =>
+  induction b using ind with | mk b =>
+  apply Quotient.sound
+  apply Pre.ext
+  exact fun x => h ⟦x⟧
 
 def Pre.lift.{u, v} : Pre.{u} -> Pre.{max u v}
 | .intro a amem => .intro (ULift a) (fun x => (amem x.down).lift)
@@ -112,5 +159,61 @@ def lift.{u, v} : ZfSet.{u} -> ZfSet.{max u v} := by
   apply (Pre.lift.spec a).symm'.trans
   apply Equiv.trans _ (Pre.lift.spec b)
   assumption
+
+def Pre.union : Pre.{u} -> Pre.{u} -> Pre.{u}
+| .intro a amem, .intro b bmem => .intro (a ⊕ b) (fun
+  | .inl x => amem x
+  | .inr x => bmem x)
+
+instance : Union Pre := ⟨Pre.union⟩
+
+def Pre.union.spec (a b c d: Pre) : a zf≈ c -> b zf≈ d -> a ∪ b zf≈ c ∪ d := by
+  cases a with | intro a amem =>
+  cases b with | intro b bmem =>
+  cases c with | intro c cmem =>
+  cases d with | intro d dmem =>
+  intro ac bd
+  apply And.intro
+  intro x
+  cases x <;> rename_i x
+  have ⟨y, prf⟩ := ac.left x
+  exists .inl y
+  have ⟨y, prf⟩ := bd.left x
+  exists .inr y
+  intro x
+  cases x <;> rename_i x
+  have ⟨y, prf⟩ := ac.right x
+  exists .inl y
+  have ⟨y, prf⟩ := bd.right x
+  exists .inr y
+
+def union : ZfSet -> ZfSet -> ZfSet := by
+  apply Quotient.lift₂ (⟦· ∪ ·⟧)
+  intros
+  apply Quotient.sound
+  apply Pre.union.spec <;> assumption
+
+instance : Union ZfSet := ⟨.union⟩
+
+@[simp]
+def mk_union {a b: Pre} : ⟦a⟧ ∪ ⟦b⟧ = ⟦a ∪ b⟧ := rfl
+
+def mem_union {a b: ZfSet} : ∀{x}, x ∈ a ∪ b ↔ x ∈ a ∨ x ∈ b := by
+  intro x
+  induction a, b, x using ind₃ with | mk a b x =>
+  cases a with | intro a amem =>
+  cases b with | intro b bmem =>
+  simp
+  apply Iff.intro
+  intro ⟨x₀, prf⟩
+  cases x₀ <;> rename_i x₀
+  left; exists x₀
+  right; exists x₀
+  intro h
+  cases h <;> rename_i h
+  obtain ⟨x, prf⟩ := h
+  exists .inl x
+  obtain ⟨x, prf⟩ := h
+  exists .inr x
 
 end ZfSet
