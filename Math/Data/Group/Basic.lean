0f67ab31ae65cbda8a126593932d867ef79ec81f
RustyYato <rustyyato@gmail.com>
2025-01-19 15:42:19 -0700

    factor out direct product

--- Math/Data/Group/Basic.lean
+++ Math/Data/Group/Basic.lean
@@ -526,54 +526,6 @@ def IsoClass.one_sub (a: IsoClass) : 1 ⊆ a := by
   apply IsNormalSubgroup.IsSubgroup
   apply one_nsub
 
-@[simp]
-def mul (a b: Group) : Group where
-  ty := a.ty × b.ty
-  one' := ⟨1, 1⟩
-  inv' | ⟨x, y⟩ => ⟨x⁻¹, y⁻¹⟩
-  mul' | ⟨a, b⟩, ⟨x, y⟩ => ⟨a * x, b * y⟩
-  mul_assoc' := by
-    intro ⟨a₀, a₁⟩ ⟨b₀, b₁⟩ ⟨c₀, c₁⟩
-    simp [mul_assoc]
-  one_mul' := by
-    intro ⟨a₀, a₁⟩
-    simp [one_mul]
-  inv_mul' := by
-    intro ⟨a₀, a₁⟩
-    simp [inv_mul_cancel]
-
-instance : Mul Group := ⟨.mul⟩
-
-def gmul_def (a b: Group) : a * b = a.mul b := rfl
-
-def gmul.spec (a b c d: Group) : a ≈ c -> b ≈ d -> a * b ≈ c * d := by
-  intro ⟨ac, ac_resp_one, ac_resp_inv, ac_resp_mul⟩
-  intro ⟨bd, bd_resp_one, bd_resp_inv, bd_resp_mul⟩
-  apply IsIsomorphic.intro (ac.toProd bd)
-  simp [Equiv.toProd]
-  erw [ac_resp_one, bd_resp_one]
-  rfl
-  intro ⟨x, y⟩
-  show (_, _) = (_, _)
-  congr
-  apply ac_resp_inv
-  apply bd_resp_inv
-  intro ⟨x₀, y₀⟩ ⟨x₁, y₁⟩
-  simp [Equiv.toProd]
-  congr
-  apply ac_resp_mul
-  apply bd_resp_mul
-
-def IsoClass.mul : IsoClass -> IsoClass -> IsoClass := by
-  apply Quotient.lift₂ (⟦· * ·⟧)
-  intros
-  apply Quot.sound
-  apply gmul.spec <;> assumption
-
-instance : Mul IsoClass := ⟨IsoClass.mul⟩
-
-def mk_mul (a b: Group) : ⟦a⟧ * ⟦b⟧ = ⟦a * b⟧ := rfl
-
 def IsSimple (a: Group) : Prop := ∀n, n ◀ a -> n ≈ 1 ∨ n ≈ a
 
 def IsSimple.spec (a b: Group) : a ≈ b -> a.IsSimple -> b.IsSimple := by
@@ -643,36 +595,6 @@ def Trivial.notNontrivial : ¬Nontrivial 1 := by
 def IsoClass.Trivial.notNontrivial : ¬Nontrivial 1 := by
   intro ⟨_, h⟩
   apply h rfl
-
-def of_gmul_eq_one (a b: Group) : a * b ≈ 1 -> a ≈ 1 ∧ b ≈ 1 := by
-  intro ⟨iso⟩
-  apply And.intro
-  · apply IsIsomorphic.intro ⟨(fun _ => ()), (fun x => (iso.invFun x).1), _, _⟩
-    rfl
-    any_goals try intro x; intros; rfl
-    intro x
-    simp [Equiv.symm]
-    show (iso.invFun 1).fst = _
-    rw [iso.inv_resp_one]
-    show 1 = x
-    symm
-    have : Prod.mk x 1 = (1: (a * b).ty) := by
-      apply iso.toFun_inj
-      rfl
-    exact (Prod.mk.inj this).left
-  · apply IsIsomorphic.intro ⟨(fun _ => ()), (fun x => (iso.invFun x).2), _, _⟩
-    rfl
-    any_goals try intro x; intros; rfl
-    intro x
-    simp [Equiv.symm]
-    show (iso.invFun 1).snd = _
-    rw [iso.inv_resp_one]
-    show 1 = x
-    symm
-    have : Prod.mk 1 x = (1: (a * b).ty) := by
-      apply iso.toFun_inj
-      rfl
-    exact (Prod.mk.inj this).right
 
 def Trivial.IsSimple : IsSimple 1 := by
   intro x nsub_one; left; symm
