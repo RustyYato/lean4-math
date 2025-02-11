d0cb8715d358dae7224d47287a63fec33015f2ac
RustyYato <rustyyato@gmail.com>
2025-02-11 12:15:58 -0700

    add the group of units and additive units

---
+++ Math/GroupTheory/Units.lean
@@ -0,0 +1,12 @@
+import Math.GroupTheory.Subgroup
+import Math.Algebra.Units
+
+namespace Group
+
+-- the group of units of a monoid
+def ofUnits (α: Type u) [MonoidOps α] [IsMonoid α] : Group (Units α) where
+
+-- the group of additive units of a add monoid
+def ofAddUnits (α: Type u) [AddMonoidOps α] [IsAddMonoid α] : Group (AddUnits α) := Group.ofAdd _
+
+end Group
