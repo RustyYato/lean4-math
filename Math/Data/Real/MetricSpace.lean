632a638de38aa16a2b72f6737ed73b1ac013288d
RustyYato <rustyyato@gmail.com>
2025-02-14 16:38:37 -0700

    generate the standard topology on the reals

--- Math/Data/Real/MetricSpace.lean
+++ Math/Data/Real/MetricSpace.lean
@@ -1,6 +1,8 @@
 import Math.Topology.MetricSpace.Abs
+import Math.Topology.MetricSpace.Basic
 import Math.Algebra.Impls.Real
 import Math.Data.Real.OrderedAlgebra
 
 instance : Dist ℝ ℝ := Abs.instDist _
 instance : IsMetricSpace ℝ := Abs.instIsMetricSpace _
+instance : Topology ℝ := Topology.ofIsPseudoMetricSpace
