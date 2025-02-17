07bbfe46915ca100ff902af79f46e84b2ea0e2a8
RustyYato <rustyyato@gmail.com>
2025-02-17 09:58:25 -0700

    refactor setoid lattice by using relation lattice and a galois insertion

--- Math/Data/Setoid/Lattice.lean
+++ Math/Data/Setoid/Lattice.lean
@@ -1,4 +1,6 @@
 import Math.Order.Lattice.Complete
+import Math.Order.GaloisConnection
+import Math.Relation.Lattice
 
 open Relation
 
@@ -10,37 +12,17 @@ instance : LE (Setoid α) where
 instance : LT (Setoid α) where
   lt a b := a ≤ b ∧ ¬b ≤ a
 
-instance : Bot (Setoid α) where
-  bot := setoid (· = ·)
-
-instance : Top (Setoid α) where
-  top := setoid trivial
-
-instance : Inf (Setoid α) where
-  inf a b := setoid (relAnd a.r b.r)
-
-instance : Sup (Setoid α) where
-  sup a b := setoid (TransGen (relOr a.r b.r))
-
-instance : InfSet (Setoid α) where
-  sInf U := {
-    r x y := ∀s ∈ U, s.r x y
-    iseqv := {
-      refl x s _ := s.iseqv.refl x
-      symm r s hs := s.iseqv.symm (r _ hs)
-      trans ab bc s hs := s.iseqv.trans (ab _ hs) (bc _ hs)
-    }
-  }
-
-def preSSup (U: Set (Setoid α)) (x y: α) : Prop := ∃s ∈ U, s.r x y
-
-instance : IsSymmetric (preSSup U) where
-  symm r := by
-    obtain ⟨s, hs, r⟩ := r
-    refine ⟨_, hs, s.iseqv.symm r⟩
-
-instance : SupSet (Setoid α) where
-  sSup U := setoid (ReflTransGen (preSSup U))
+def giRelation : @GaloisInsertion (α -> α -> Prop) (Setoid α) _ _ (fun r => setoid (EquivGen r)) (fun s => s.r) where
+  gc := by
+    intro r s
+    dsimp
+    apply Iff.intro
+    intro h x y g
+    exact h (EquivGen.single g)
+    intro h x y g
+    refine ofMappedEquivGen (s := s.r) (g := ⟨id, h⟩) g
+  le_l_u s := by
+    apply EquivGen.single
 
 instance : IsPartialOrder (Setoid α) where
   lt_iff_le_and_not_le := Iff.rfl
@@ -56,68 +38,4 @@ instance : IsPartialOrder (Setoid α) where
     apply ab
     apply ba
 
-instance : IsCompleteLattice (Setoid α) where
-  bot_le := by
-    intro a x y r
-    subst y
-    rfl
-  le_top := by
-    intro _ _ _ _
-    trivial
-  le_sup_left := by
-    intro a b x y r
-    apply TransGen.single
-    left; assumption
-  le_sup_right := by
-    intro a b x y r
-    apply TransGen.single
-    right; assumption
-  sup_le := by
-    intro a b k ak bk x y r
-    induction r with
-    | single r =>
-      rcases r with r | r
-      apply ak
-      assumption
-      apply bk
-      assumption
-    | tail _ r =>
-      apply Relation.trans
-      assumption
-      rcases r with r | r
-      apply ak
-      assumption
-      apply bk
-      assumption
-  inf_le_left := by
-    intro a b x y r
-    exact r.left
-  inf_le_right := by
-    intro a b x y r
-    exact r.right
-  le_inf := by
-    intro a b k ak bk x y r
-    exact ⟨ak r, bk r⟩
-  le_sSup := by
-    intro U s hs x y r
-    apply ReflTransGen.single
-    exists s
-  sSup_le := by
-    intro k U h x y r
-    induction r with
-    | refl => rfl
-    | cons r _ ih =>
-      apply Relation.trans _ ih
-      obtain ⟨s, hs, r⟩ := r
-      apply h
-      assumption
-      assumption
-  sInf_le := by
-    intro U s hs x y r
-    apply r
-    assumption
-  le_sInf := by
-    intro k U h x y r s hs
-    apply h
-    assumption
-    assumption
+instance : CompleteLattice (Setoid α) := giRelation.liftCompleteLattice
