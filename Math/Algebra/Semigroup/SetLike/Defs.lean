3beab1451e3e41e67a5dfe8689e717f66e9189f5
RustyYato <rustyyato@gmail.com>
2025-02-20 11:04:06 -0700

    add compatibility instances to swap between mul and add

--- Math/Algebra/Semigroup/SetLike/Defs.lean
+++ Math/Algebra/Semigroup/SetLike/Defs.lean
@@ -1,4 +1,5 @@
 import Math.Data.Set.Like
+import Math.Algebra.AddMul
 
 variable (S: Type*) {α: Type*} [SetLike S α]
 
@@ -37,3 +38,15 @@ instance [Add α] : SetLike (SubAddSemigroup α) α where
 
 instance [Add α] : IsAddMem (SubAddSemigroup α) where
   mem_add := SubAddSemigroup.mem_add'
+
+instance [h: SetLike S α] : SetLike (AddOfMul S) (AddOfMul α) where
+  coe a := (a.get: Set α)
+  coe_inj := h.coe_inj
+instance [h: SetLike S α] : SetLike (MulOfAdd S) (MulOfAdd α) where
+  coe a := (a.get: Set α)
+  coe_inj := h.coe_inj
+
+instance [SetLike S α] [Add α] [IsAddMem S] : IsMulMem (MulOfAdd S) where
+  mem_mul := mem_add (S := S)
+instance [SetLike S α] [Mul α] [IsMulMem S] : IsAddMem (AddOfMul S) where
+  mem_add := mem_mul (S := S)
