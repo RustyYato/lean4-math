f66e4c1c4db4ddd92e10313062e603d2cc0263f0
RustyYato <rustyyato@gmail.com>
2025-02-24 20:38:09 -0700

    refactor sum to put start at the front

--- Math/Data/Fin/Sum.lean
+++ Math/Data/Fin/Sum.lean
@@ -8,7 +8,7 @@ namespace Fin
 
 def sum_from [Add α] (start: α) : ∀{n}, (Fin n -> α) -> α
 | 0, _ => start
-| _ + 1, f => f 0 + sum_from start (f ∘ Fin.succ)
+| _ + 1, f => sum_from start (f ∘ Fin.castSucc) + f (last _)
 
 def sum [Zero α] [Add α] : ∀{n}, (Fin n -> α) -> α := sum_from 0
 
@@ -20,45 +20,49 @@ def sum_from_zero [Add α] (start: α) (f: Fin 0 -> α) : sum_from start f = sta
 
 def prod_from_zero [Mul α] (start: α) (f: Fin 0 -> α) : prod_from start f = start := rfl
 
-def sum_from_succ [Add α] (start: α) (f: Fin (n + 1) -> α) : sum_from start f = f 0 + sum_from start (f ∘ Fin.succ)  := rfl
+def sum_from_succ [Add α] (start: α) (f: Fin (n + 1) -> α) : sum_from start f = sum_from start (f ∘ Fin.castSucc) + f (last _)  := rfl
 
-def prod_from_succ [Mul α] (start: α) (f: Fin (n + 1) -> α) : prod_from start f = f 0 * prod_from start (f ∘ Fin.succ)  := rfl
+def prod_from_succ [Mul α] (start: α) (f: Fin (n + 1) -> α) : prod_from start f = prod_from start (f ∘ Fin.castSucc) * f (last _) := rfl
 
 def sum_zero [Zero α] [Add α] (f: Fin 0 -> α) : sum f = 0 := rfl
 
 def prod_zero [One α] [Mul α] (f: Fin 0 -> α) : prod f = 1 := rfl
 
-def sum_succ [Zero α] [Add α] (f: Fin (n + 1) -> α) : sum f = f 0 + sum (f ∘ Fin.succ) := rfl
+def sum_succ [Zero α] [Add α] (f: Fin (n + 1) -> α) : sum f = sum (f ∘ Fin.castSucc) + f (last _) := rfl
 
-def prod_succ [One α] [Mul α] (f: Fin (n + 1) -> α) : prod f = f 0 * prod (f ∘ Fin.succ) := rfl
+def prod_succ [One α] [Mul α] (f: Fin (n + 1) -> α) : prod f = prod (f ∘ Fin.castSucc) * f (last _) := rfl
 
 def sum_from_eq_sum [Zero α] [Add α] [IsAddSemigroup α] [IsAddZeroClass α] (start: α) (f: Fin n -> α) :
-  sum_from start f = sum f + start := by
+  sum_from start f = start + sum f := by
   induction n with
-  | zero => symm; apply zero_add
+  | zero => symm; apply add_zero
   | succ n ih => rw [sum_from_succ, sum_succ, ih, add_assoc]
 
 def prod_from_eq_prod [One α] [Mul α] [IsSemigroup α] [IsMulOneClass α] (start: α) (f: Fin n -> α) :
-  prod_from start f = prod f * start :=
+  prod_from start f = start * prod f :=
   sum_from_eq_sum (α := AddOfMul α) _ _
 
 def sum_from_succ' [Add α] [IsAddSemigroup α] (start: α) (f: Fin (n + 1) -> α) :
-  sum_from start f = sum_from (f (last _)) (fun x => f x.castSucc) + start := by
+  sum_from start f = start + sum_from (f 0) (f ∘ Fin.succ) := by
   induction n with
   | zero => rfl
   | succ n ih =>
-    rw [sum_from_succ, ih, ←add_assoc]
+    rw [sum_from_succ, ih, sum_from_succ, ←add_assoc]
     rfl
 
 def sum_succ' [Zero α] [Add α] [IsAddSemigroup α] [IsAddZeroClass α] (f: Fin (n + 1) -> α) :
-  sum f = sum (fun x => f x.castSucc) + f (last _) := by
+  sum f = f 0 + sum (f ∘ Fin.succ) := by
   unfold sum
-  rw [sum_from_succ', add_zero, sum_from_eq_sum]
+  rw [sum_from_succ', sum_from_eq_sum, zero_add]
   rfl
 
+def prod_from_succ' [Mul α] [IsSemigroup α] (start: α) (f: Fin (n + 1) -> α) :
+  prod_from start f = start * prod_from (f 0) (f ∘ Fin.succ) :=
+  sum_from_succ' (α := AddOfMul α) _ _
+
 def prod_succ' [One α] [Mul α] [IsSemigroup α] [IsMulOneClass α] (f: Fin (n + 1) -> α) :
-  prod f = prod (fun x => f x.castSucc) * f (last _) :=
-  sum_succ' (α := AddOfMul α) f
+  prod f = f 0 * prod (f ∘ Fin.succ) :=
+  sum_succ' (α := AddOfMul α) _
 
 end Fin
 
@@ -86,21 +90,46 @@ end Hom
 
 namespace Fin
 
-def sum_from_add_sum_from [Add α] [IsAddSemigroup α] [IsAddCommMagma α] (fs gs: α) (f g: Fin n -> α) : Fin.sum_from fs f + Fin.sum_from gs g = Fin.sum_from (fs + gs) (f + g) := by
+def sum_from_ext [Add α] (fs gs: α) (f g: Fin n -> α) :
+  fs = gs ->
+  (∀x, f x = g x) ->
+  Fin.sum_from fs f = Fin.sum_from gs g := by
+  rintro rfl h
+  induction n with
+  | zero => rfl
+  | succ n ih =>
+    rw [sum_from_succ, sum_from_succ, h, ih]
+    intro x
+    apply h
+
+def sum_ext [Zero α] [Add α] (f g: Fin n -> α) :
+  (∀x, f x = g x) ->
+  Fin.sum f = Fin.sum g := by
+  apply sum_from_ext
+  rfl
+
+def sum_from_add_sum_from_pairwise [Add α] [IsAddSemigroup α] [IsAddCommMagma α] (fs gs: α) (f g: Fin n -> α) : Fin.sum_from fs f + Fin.sum_from gs g = Fin.sum_from (fs + gs) (f + g) := by
   induction n with
   | zero => rfl
   | succ n ih =>
     simp only [sum_from_succ]
-    rw [add_assoc, add_left_comm _ (g _), ←add_assoc, ih]
+    rw [add_assoc, add_left_comm _ _ (g _), ←add_assoc, ih]
     rfl
 
-def sum_add_sum [Zero α] [Add α] [IsAddZeroClass α] [IsAddSemigroup α] [IsAddCommMagma α] (f g: Fin n -> α) : Fin.sum f + Fin.sum g = Fin.sum (f + g) := by
-  rw [sum, sum_from_add_sum_from, add_zero]
+def sum_add_sum_pairwise [Zero α] [Add α] [IsAddZeroClass α] [IsAddSemigroup α] [IsAddCommMagma α] (f g: Fin n -> α) : Fin.sum f + Fin.sum g = Fin.sum (f + g) := by
+  rw [sum, sum_from_add_sum_from_pairwise, add_zero]
 
-def prod_from_mul_prod_from [Mul α] [IsSemigroup α] [IsCommMagma α] (fs gs: α) (f g: Fin n -> α) : Fin.prod_from fs f * Fin.prod_from gs g = Fin.prod_from (fs * gs) (f * g) :=
-  sum_from_add_sum_from (α := AddOfMul α) _ _ _ _
+def prod_from_mul_prod_from_pairwise [Mul α] [IsSemigroup α] [IsCommMagma α] (fs gs: α) (f g: Fin n -> α) : Fin.prod_from fs f * Fin.prod_from gs g = Fin.prod_from (fs * gs) (f * g) :=
+  sum_from_add_sum_from_pairwise (α := AddOfMul α) _ _ _ _
 
-def prod_mul_prod [One α] [Mul α] [IsMulOneClass α] [IsSemigroup α] [IsCommMagma α] (f g: Fin n -> α) : Fin.prod f * Fin.prod g = Fin.prod (f * g) :=
-  sum_add_sum (α := AddOfMul α) _ _
+def prod_mul_prod_pairwise [One α] [Mul α] [IsMulOneClass α] [IsSemigroup α] [IsCommMagma α] (f g: Fin n -> α) : Fin.prod f * Fin.prod g = Fin.prod (f * g) :=
+  sum_add_sum_pairwise (α := AddOfMul α) _ _
+
+def add_apend [Add α] (f: Fin n -> α) (g: Fin m -> α) : Fin (n + m) -> α :=
+  fun x =>
+    if h:x < n then f ⟨x, h⟩  else g ⟨x.val - n, by
+      refine Nat.sub_lt_left_of_lt_add ?_ ?_
+      exact Nat.le_of_not_lt h
+      exact x.isLt⟩
 
 end Fin
