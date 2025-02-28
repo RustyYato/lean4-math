529534033a6c3b8a463b4d0e456a9e31510a1631
RustyYato <rustyyato@gmail.com>
2025-02-28 11:02:45 -0700

    refactor core ZMod proofs

--- Math/Algebra/Ring/ZMod.lean
+++ Math/Algebra/Ring/ZMod.lean
@@ -46,120 +46,91 @@ def zmod_zero_eqv_int : ZMod 0 ≃+* ℤ :=
     rintro rfl
     apply Int.dvd_refl
 
-def zmod_succ_eqv_fin (n: Nat) [h: NeZero n] : ZMod n ≃+* Fin n where
-  invFun x := Ideal.mkQuot _ x.val
-  toFun := by
-    have n_ne_zero : n ≠ 0 := Ne.symm (NeZero.ne' n)
-    apply Quotient.lift (fun x: ℤ => Fin.mk (x % n).toNat (by
-      refine (Int.toNat_lt ?_).mpr ?_
-      refine Int.emod_nonneg x ?_
+private def int_dvd_emod_sub (n x: Int) : n ∣ (x % n) - x := by
+  conv => { rhs; rhs; rw [←Int.ediv_add_emod x n] }
+  rw [sub_eq_add_neg, neg_add_rev, ←add_assoc, add_neg_cancel, zero_add]
+  apply Int.dvd_neg.mpr
+  apply Int.dvd_mul_right
+
+def zmod_succ_eqv_fin (n: Nat) [h: NeZero n] : ZMod n ≃+* Fin n :=
+  RingEquiv.symm <| {
+    toFun x := x.val
+    invFun x := by
+      have n_ne_zero : n ≠ 0 := Ne.symm (NeZero.ne' n)
+      apply x.lift (fun x: ℤ => Fin.mk (x % n).toNat (by
+        refine (Int.toNat_lt ?_).mpr ?_
+        refine Int.emod_nonneg x ?_
+        omega
+        refine Int.emod_lt_of_pos x ?_
+        omega))
+      intro a b eqv
+      apply Fin.val_inj.mp
+      apply Int.ofNat.inj
+      show ((a % ↑n).toNat: ℤ) = ↑(b % ↑n).toNat
+      rw [Int.toNat_of_nonneg, Int.toNat_of_nonneg]
+      · have dvd: ↑n ∣ a - b := eqv
+        obtain ⟨k, eq⟩ := dvd
+        replace eq : a = n * k + b := by rw [←eq, sub_add_cancel]
+        subst eq
+        rw [Int.add_emod, Int.mul_emod_right, Int.zero_add,
+          Int.emod_emod]
+      · refine Int.emod_nonneg b ?_
+        omega
+      · refine Int.emod_nonneg a ?_
+        omega
+    leftInv := by
+      intro x
+      simp
+      apply Fin.val_inj.mp
+      let mkQuot : ℤ →+* ZMod n := (Int.multiples n).mkQuot
+      rw [←resp_natCast mkQuot]
+      show Int.toNat ((x.val: ℤ) % n) = x.val
+      apply Int.ofNat.inj
+      rw [Int.ofNat_eq_coe, Int.ofNat_eq_coe, Int.toNat_of_nonneg]
+      rw [←Int.ofNat_emod, Nat.mod_eq_of_lt]
+      exact x.isLt
+      apply Int.emod_nonneg
+      have := h.out
       omega
-      refine Int.emod_lt_of_pos x ?_
-      omega))
-    intro a b eqv
-    replace eqv: (n: ℤ) ∣ a - b := eqv
-    congr 1
-    apply Int.ofNat_inj.mp
-    rw [Int.toNat_of_nonneg, Int.toNat_of_nonneg]
-    · rw [←Int.ediv_add_emod a n, ←Int.ediv_add_emod b n,
-        sub_add] at eqv
-      rw [sub_eq_add_neg, add_comm, add_sub_assoc, ←add_assoc, add_comm (-_),
-        ←sub_eq_add_neg, ←mul_sub] at eqv
-      replace eqv := (Int.dvd_iff_dvd_of_dvd_add eqv).mp (Int.dvd_mul_right _ _)
-      obtain ⟨x, hx⟩ := eqv
-      apply eq_of_sub_eq_zero
-      rw [hx]
-      refine Int.mul_eq_zero.mpr ?_; right
-      have : (a % n - b % n).natAbs < n := by
-        apply Int.ofNat_lt.mp
-        rcases le_total (a % n) (b % n) with h | h
-        rw [←Int.natAbs_neg, neg_sub, Int.natAbs_of_nonneg]
-        refine Int.sub_lt_iff.mpr ?_
-        apply lt_of_lt_of_le
-        apply Int.emod_lt_of_pos
-        omega
-        apply Int.le_add_of_nonneg_right
-        refine Int.emod_nonneg a ?_
-        omega
-        omega
-        rw [Int.natAbs_of_nonneg]
-        refine Int.sub_lt_iff.mpr ?_
-        apply lt_of_lt_of_le
-        apply Int.emod_lt_of_pos
-        omega
-        apply Int.le_add_of_nonneg_right
-        refine Int.emod_nonneg b ?_
-        omega
-        omega
-      match x with
-      | 0 => rfl
-      | .ofNat (x + 1) =>
-        replace hx : _ = _ * ((x + 1: ℕ): ℤ) := hx
-        rw [Int.ofNat_succ, mul_add, mul_one] at hx
-        rw [hx, ←Int.ofNat_mul, ←Int.ofNat_add, Int.natAbs_ofNat] at this
-        omega
-      | .negSucc x =>
-        rw [hx, Int.natAbs_mul, Int.natAbs_negSucc, Int.natAbs_ofNat,
-          Nat.mul_succ] at this
-        omega
-    refine Int.emod_nonneg _ ?_
-    omega
-    refine Int.emod_nonneg _ ?_
-    omega
-  rightInv := by
-    intro x
-    apply Fin.val_inj.mp
-    show x.val % n = x
-    rw [Nat.mod_eq_of_lt]
-    apply Fin.isLt
-  leftInv := by
-    intro x
-    induction x using Quot.ind with | mk x =>
-    apply Quotient.sound
-    dsimp
-    show (n: ℤ) ∣ _ - _
-    rw [Int.toNat_of_nonneg]
-    conv => { rhs; rhs; rw [←Int.ediv_add_emod x n] }
-    rw [sub_eq_add_neg, neg_add_rev, ←add_assoc, add_neg_cancel, zero_add]
-    apply Int.dvd_neg.mpr
-    apply Int.dvd_mul_right
-    refine Int.emod_nonneg _ ?_
-    refine Int.ofNat_ne_zero.mpr ?_
-    exact Ne.symm (NeZero.ne' n)
-  resp_zero := rfl
-  resp_one := rfl
-  resp_add := by
-    intro a b
-    dsimp
-    cases a using Quotient.ind
-    cases b using Quotient.ind
-    rename_i a b
-    show Quotient.lift _ _ (Quotient.mk _ (a + b)) = _
-    rw [Quotient.mk_lift, Quotient.mk_lift, Quotient.mk_lift]
-    congr
-    have n_ne_zero : n ≠ 0 := Ne.symm (NeZero.ne' n)
-    apply Int.ofNat_inj.mp
-    erw [←Int.ofNat_mod_ofNat, Int.ofNat_add, Int.toNat_of_nonneg,
-      Int.toNat_of_nonneg, Int.toNat_of_nonneg, Int.add_emod]
-    all_goals
-    refine Int.emod_nonneg _ ?_
-    omega
-  resp_mul := by
-    intro a b
-    dsimp
-    cases a using Quotient.ind
-    cases b using Quotient.ind
-    rename_i a b
-    show Quotient.lift _ _ (Quotient.mk _ (a * b)) = _
-    rw [Quotient.mk_lift, Quotient.mk_lift, Quotient.mk_lift]
-    congr
-    have n_ne_zero : n ≠ 0 := Ne.symm (NeZero.ne' n)
-    apply Int.ofNat_inj.mp
-    erw [←Int.ofNat_mod_ofNat, Int.ofNat_mul, Int.toNat_of_nonneg,
-      Int.toNat_of_nonneg, Int.toNat_of_nonneg, Int.mul_emod]
-    all_goals
-    refine Int.emod_nonneg _ ?_
-    omega
+    rightInv := by
+      intro x
+      obtain ⟨x, rfl⟩ := (Int.multiples n).mkQuot_surj x
+      dsimp
+      let mkQuot : ℤ →+* ZMod n := (Int.multiples n).mkQuot
+      rw [←resp_natCast mkQuot]
+      apply Quotient.sound
+      show (n: ℤ) ∣ ↑(x % ↑n).toNat - x
+      rw [Int.toNat_of_nonneg]
+      apply int_dvd_emod_sub
+      refine Int.emod_nonneg x ?_
+      have := h.out
+      omega
+    resp_zero := rfl
+    resp_one := by
+      match n, h with
+      | 1, h =>
+        apply Quotient.sound
+        apply Int.one_dvd
+      | n + 2, h => rfl
+    resp_add {x y} := by
+      show (((x.val + y.val) % n): ZMod n) = (x.val: ZMod n) + (y.val: ZMod n)
+      rw [←natCast_add]
+      let mkQuot : ℤ →+* ZMod n := (Int.multiples n).mkQuot
+      rw [←resp_natCast mkQuot, ←resp_natCast mkQuot]
+      apply Quotient.sound
+      show (n: ℤ) ∣ _
+      rw [Int.ofNat_emod]
+      apply int_dvd_emod_sub
+    resp_mul {x y} := by
+      show (((x.val * y.val) % n): ZMod n) = (x.val: ZMod n) * (y.val: ZMod n)
+      rw [←natCast_mul]
+      let mkQuot : ℤ →+* ZMod n := (Int.multiples n).mkQuot
+      rw [←resp_natCast mkQuot, ←resp_natCast mkQuot]
+      apply Quotient.sound
+      show (n: ℤ) ∣ _
+      rw [Int.ofNat_emod]
+      apply int_dvd_emod_sub
+  }
 
 instance : IsCommMagma (ZMod n) where
   mul_comm := by
@@ -245,17 +216,25 @@ def n_zsmul (a: ZMod n) : (n: ℤ) • a = 0 := by
   simp [zsmul_eq_intCast_mul, intCast_ofNat]
 
 @[simp]
-def natCast_eq_natCast (a b: ℕ) [h: NeZero n] :
+def natCast_eq_natCast (a b: ℕ) :
   a % n = b % n ->
   (a: ZMod n) = (b: ZMod n) := by
   intro g
-  apply (zmod_succ_eqv_fin n).inj
+  cases n with
+  | zero =>
+    rw [Nat.mod_zero, Nat.mod_zero] at g
+    congr
+  | succ n =>
+  apply (zmod_succ_eqv_fin (n+1)).inj
   show zmod_succ_eqv_fin _ _ = zmod_succ_eqv_fin _ _
-  match n, h with
-  | n + 1, h =>
   rw [resp_natCast, resp_natCast]
   apply Fin.val_inj.mp
   assumption
+
+def natCast_mod_n (a: ℕ) : ((a % n): ZMod n) = a := by
+  conv => { rhs; rw [←Nat.div_add_mod a n] }
+  rw [natCast_add, natCast_mul]
+  simp [n_eq_zero]
 
 def homOfDvd (n m: ℕ) (h: m ∣ n) : ZMod n →+* ZMod m where
   toFun n := toInt n
@@ -275,49 +254,27 @@ def homOfDvd (n m: ℕ) (h: m ∣ n) : ZMod n →+* ZMod m where
     decide
     rename_i n
     rfl
-  resp_add := by
-    intro x y
-    dsimp
-    cases n <;> unfold toInt <;> dsimp
+  resp_add {x y} := by
+    cases n
+    unfold toInt
+    dsimp only
     rw [resp_add, intCast_add]
-    rw [resp_add]
-    rename_i n
-    generalize zmod_succ_eqv_fin (n + 1) x = x'
-    generalize zmod_succ_eqv_fin (n + 1) y = y'
-    rw [intCast_ofNat, intCast_ofNat, intCast_ofNat, ←natCast_add]
-    have : NeZero m := {
-      out := by
-        rintro rfl
-        have := Nat.zero_dvd.mp h
-        contradiction
-    }
-    apply natCast_eq_natCast
-    clear x y
-    rw [Fin.add_def]
-    dsimp
-    rw [Nat.mod_mod_of_dvd]
+    unfold toInt
+    dsimp only
+    rw [resp_add, intCast_ofNat, intCast_ofNat, intCast_ofNat,
+      Fin.add_def, Fin.val_mk]
+    rw [←natCast_mod_n, Nat.mod_mod_of_dvd, natCast_mod_n, natCast_add]
     assumption
-  resp_mul := by
-    intro x y
-    dsimp
-    cases n <;> unfold toInt <;> dsimp
+  resp_mul {x y} := by
+    cases n
+    unfold toInt
+    dsimp only
     rw [resp_mul, intCast_mul]
-    rw [resp_mul]
-    rename_i n
-    generalize zmod_succ_eqv_fin (n + 1) x = x'
-    generalize zmod_succ_eqv_fin (n + 1) y = y'
-    rw [intCast_ofNat, intCast_ofNat, intCast_ofNat, ←natCast_mul]
-    have : NeZero m := {
-      out := by
-        rintro rfl
-        have := Nat.zero_dvd.mp h
-        contradiction
-    }
-    apply natCast_eq_natCast
-    clear x y
-    rw [Fin.mul_def]
-    dsimp
-    rw [Nat.mod_mod_of_dvd]
+    unfold toInt
+    dsimp only
+    rw [resp_mul, intCast_ofNat, intCast_ofNat, intCast_ofNat,
+      Fin.mul_def, Fin.val_mk]
+    rw [←natCast_mod_n, Nat.mod_mod_of_dvd, natCast_mod_n, natCast_mul]
     assumption
 
 instance : HasChar (ZMod n) n :=
