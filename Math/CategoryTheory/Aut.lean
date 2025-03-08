8745159db57264d51a9134094522d22e39a455ba
RustyYato <rustyyato@gmail.com>
2025-03-08 11:27:29 -0700

    define the automorphism group as the group of units

--- Math/CategoryTheory/Aut.lean
+++ Math/CategoryTheory/Aut.lean
@@ -1,66 +1,57 @@
-import Math.CategoryTheory.Basic
+import Math.CategoryTheory.Functor.Basic
 import Math.Algebra.Group.Theory.Basic
+import Math.Algebra.Group.Units.Defs
 
 namespace Category
 
-variable [Category α] {x: α}
+variable [Category α] [Category β] {x: α}
 
-structure Aut (x: α) where
-  toFun: x ⟶ x
-  invFun: x ⟶ x
-  leftInv: invFun ≫ toFun = 𝟙 x
-  rightInv: toFun ≫ invFun = 𝟙 x
+local instance : One (x ⟶ x) where
+  one := 𝟙 _
+local instance : Mul (x ⟶ x) where
+  mul := (· ≫ ·)
 
-@[ext]
-def Aut.ext (a b: Aut x) : a.toFun = b.toFun -> a = b := by
-  intro h
-  suffices a.invFun = b.invFun by
-    cases a; cases b; congr
-  have : b.invFun ≫ (a.toFun ≫ a.invFun) = b.invFun ≫ (b.toFun ≫ b.invFun) := by simp [a.rightInv, b.rightInv]
-  rw [h, comp_assoc, comp_assoc, b.leftInv] at this
-  simp at this
-  assumption
+instance : MonoidOps (x ⟶ x) where
+  npow := flip npowRec
+instance : IsMonoid (x ⟶ x) where
+  mul_assoc _ _ _ := (comp_assoc _ _ _).symm
+  one_mul _ := id_comp _
+  mul_one _ := comp_id _
 
-def group : Group (Aut x) := by
-  apply Group.ofAxiomsLeft
-  case one => exact {
-    toFun := 𝟙 _
-    invFun := 𝟙 _
-    leftInv := by simp
-    rightInv := by simp
+def Aut (x: α) := Units (x ⟶ x)
+
+instance : GroupOps (Aut x) := inferInstanceAs (GroupOps (Units _))
+instance : IsGroup (Aut x) := inferInstanceAs (IsGroup (Units _))
+
+-- every functor from α to β induces a group homomorphism from
+-- between their automorphism groups
+def groupHom_of_functor (f: α ⥤ β) (x: α) : Aut x →* Aut (f.obj x) where
+  toFun x := {
+    val := f.map x.val
+    inv := f.map x.inv
+    val_mul_inv := by
+      show _ ≫ _ = _
+      rw [←f.map_comp]
+      show f.map (x.val * _) = _
+      rw [x.val_mul_inv]
+      show f.map (𝟙 _) = _
+      rw [f.map_id]
+      rfl
+    inv_mul_val := by
+      show _ ≫ _ = _
+      rw [←f.map_comp]
+      show f.map (x.inv * _) = _
+      rw [x.inv_mul_val]
+      show f.map (𝟙 _) = _
+      rw [f.map_id]
+      rfl
   }
-  case inv =>
-    intro h
-    exact {
-      toFun := h.invFun
-      invFun := h.toFun
-      leftInv := h.rightInv
-      rightInv := h.leftInv
-    }
-  case mul =>
-    intro h g
-    exact {
-      toFun := h.toFun ≫ g.toFun
-      invFun := g.invFun ≫ h.invFun
-      leftInv := by
-        rw [comp_assoc, ←comp_assoc g.invFun]
-        simp [g.leftInv,h.leftInv]
-      rightInv := by
-        rw [comp_assoc, ←comp_assoc h.toFun]
-        simp [g.rightInv, h.rightInv]
-    }
-  intro x
-  ext
-  show 𝟙 _ ≫ _ = _
-  simp
-  intro m
-  ext
-  apply m.leftInv
-  intro a b c
-  ext
-  symm; apply comp_assoc
-
-instance : GroupOps (Aut x) := group.ops
-instance : IsGroup (Aut x) := group.spec
+  resp_one := by
+    apply Units.val.inj
+    apply f.map_id
+  resp_mul := by
+    intro a b
+    apply Units.val.inj
+    apply f.map_comp
 
 end Category
