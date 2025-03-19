99bf94807c2ef014fbac0352d0f296247989b93c
RustyYato <rustyyato@gmail.com>
2025-03-18 17:02:48 -0700

    simplify has char proof for poly

--- Math/Data/Poly/Basic.lean
+++ Math/Data/Poly/Basic.lean
@@ -6,11 +6,4 @@ variable [SemiringOps P] [IsSemiring P]
 
 open Poly
 
-instance [HasChar P n] : HasChar P[X] n := by
-  apply HasChar.of_natCast_eq_zero
-  rw [←resp_natCast (Poly.C (P := P))]
-  rw [HasChar.natCast_eq_zero, resp_zero]
-  intro m h
-  rw [←resp_natCast (Poly.C (P := P)), ←resp_zero (Poly.C (P := P))] at h
-  have := C.inj h
-  exact HasChar.char_dvd_natCast_eq_zero m this
+instance [HasChar P n] : HasChar P[X] n := HasChar.of_ring_emb C
