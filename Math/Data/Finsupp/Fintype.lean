c9320fc1a6d7b9c31b78fa59e10da7520d5c72f2
RustyYato <rustyyato@gmail.com>
2025-04-22 10:10:52 -0700

    fix sum_eq_fintype

--- Math/Data/Finsupp/Fintype.lean
+++ Math/Data/Finsupp/Fintype.lean
@@ -9,25 +9,23 @@ def sum_eq_fintypesum
   [Zero α] [∀a: α, Decidable (a = 0)] [AddMonoidOps γ] [IsAddCommMagma γ] [IsAddMonoid γ] (f: Finsupp ι α S) (g: ι -> α -> γ) (resp: ∀i: ι, f i = 0 -> g i (f i) = 0):
   f.sum g resp = ∑i: f.support, g i.val (f i.val) := by
   rw [Finsupp.sum_eq_support_sum]
-  -- FIXME: redo proof
-  sorry
-  -- unfold _root_.sum
-  -- show _ = ((Multiset.map ((fun i => g i (f i)) ∘ (fun i: f.support => i.val))) _).sum
-  -- rw [←Multiset.map_map]
-  -- congr
-  -- unfold Finset.univ Fintype.all instFintypeElem
-  -- simp
-  -- suffices f.support = f.support.attach.map_emb Embedding.subtypeVal by
-  --   rw (occs := [1]) [this]
-  --   rfl
-  -- ext i
-  -- simp [Finset.mem_map_emb]
-  -- apply Iff.intro
-  -- intro h
-  -- refine ⟨i, h, ?_, ?_⟩
-  -- apply Finset.mem_attach
-  -- rfl
-  -- rintro ⟨i, h, _, rfl⟩
-  -- assumption
+  unfold _root_.sum
+  show _ = ((Multiset.map ((fun i => g i (f i)) ∘ (fun i: f.support => i.val))) _).sum
+  rw [←Multiset.map_map]
+  congr
+  unfold Finset.univ Fintype.all instFintypeElem
+  simp
+  suffices f.support = f.support.attach.map_emb Embedding.subtypeVal by
+    rw (occs := [1]) [this]
+    rfl
+  ext i
+  simp [Finset.mem_map_emb]
+  apply Iff.intro
+  intro h
+  refine ⟨?_, ?_⟩
+  assumption
+  apply Finset.mem_attach
+  intro ⟨_, _⟩
+  assumption
 
 end Finsupp
