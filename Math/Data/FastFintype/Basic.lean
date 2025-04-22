137c6af1ee1457084d77b962ae19eb40d4f1ffcf
RustyYato <rustyyato@gmail.com>
2025-04-22 10:09:16 -0700

    remove FastFintype this is the wrong layer of abstraction, instead I should create a lazy list and replace the list in Fintype

--- Math/Data/FastFintype/Basic.lean
+++
@@ -1,7 +0,0 @@
-import Math.Data.FastFintype.Defs
-import Math.Data.FastFintype.Impls.Pi
-import Math.Data.FastFintype.Impls.Sigma
-import Math.Data.FastFintype.Impls.Prod
-import Math.Data.FastFintype.Impls.Sum
-import Math.Data.FastFintype.Impls.Subtype
-import Math.Data.FastFintype.Induction
