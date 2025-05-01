35a069e9d5a50323656e4a6b1f11b00fbdacca53
RustyYato <rustyyato@gmail.com>
2025-04-30 20:38:16 -0700

    define a more clean version of ordinals

---
+++ Math/Data/Ordinal/Defs.lean
@@ -0,0 +1,106 @@
+import Math.Relation.RelIso
+import Math.Tactics.PPWithUniv
+import Math.Relation.Segments
+
+namespace Ordinal
+
+universe u v w
+
+@[pp_with_univ]
+structure Pre: Type (u + 1) where
+  ty: Type u
+  rel: ty -> ty -> Prop
+  well_order: Relation.IsWellOrder rel := by infer_instance
+
+instance (p: Pre) : Relation.IsWellOrder p.rel := p.well_order
+
+instance pre_setoid : Setoid Pre where
+  r a b := Nonempty (a.rel ≃r b.rel)
+  iseqv := {
+    refl _ := ⟨RelIso.refl⟩
+    symm | ⟨h⟩ => ⟨h.symm⟩
+    trans | ⟨h⟩, ⟨g⟩ => ⟨h.trans g⟩
+  }
+
+@[pp_with_univ]
+def _root_.Ordinal := Quotient pre_setoid
+
+def type {α: Type u} (rel: α -> α -> Prop) [Relation.IsWellOrder rel] : Ordinal := Quotient.mk _ (Pre.mk _ rel)
+def type' {α: Type u} (rel: α -> α -> Prop) (is_well_order: Relation.IsWellOrder rel) : Ordinal := type rel
+
+@[induction_eliminator]
+def ind {motive : Ordinal -> Prop} (typein: ∀(α: Type u) (rel: α -> α -> Prop) [Relation.IsWellOrder rel], motive (type rel)) (o: Ordinal) : motive o := by
+  induction o using Quotient.ind with | _ o =>
+  apply typein
+
+def sound {α β: Type u} (relα: α -> α -> Prop) (relβ: β -> β -> Prop) (hrelα: Relation.IsWellOrder relα := by infer_instance) (hrelβ: Relation.IsWellOrder relβ := by infer_instance) :
+  relα ≃r relβ -> type relα = type relβ := by intro h; exact Quotient.sound ⟨h⟩
+
+def lift (f: ∀(α: Type u) (relα: α -> α -> Prop) [Relation.IsWellOrder relα], γ) (hf: ∀(α β: Type u) (relα: α -> α -> Prop) (relβ: β -> β -> Prop) [Relation.IsWellOrder relα] [Relation.IsWellOrder relβ], relα ≃r relβ -> f α relα = f β relβ) : Ordinal -> γ := by
+  refine Quotient.lift (fun p => f p.ty p.rel) ?_
+  intro a b ⟨h⟩; apply hf
+  assumption
+
+def lift₂ (f: ∀(α β: Type u) (relα: α -> α -> Prop) (relβ: β -> β -> Prop) [Relation.IsWellOrder relα] [Relation.IsWellOrder relβ], γ)
+  (hf: ∀(α β γ δ: Type u)
+    (relα: α -> α -> Prop) (relβ: β -> β -> Prop)
+    (relγ: γ -> γ -> Prop) (relδ: δ -> δ -> Prop)
+    [Relation.IsWellOrder relα] [Relation.IsWellOrder relβ]
+    [Relation.IsWellOrder relγ] [Relation.IsWellOrder relδ],
+    relα ≃r relγ ->
+    relβ ≃r relδ -> f α β relα relβ = f γ δ relγ relδ) : Ordinal -> Ordinal -> γ := by
+  refine Quotient.lift₂ (fun p q => f _ _ p.rel q.rel) ?_
+  intro a b c d ⟨h⟩ ⟨g⟩; apply hf
+  assumption
+  assumption
+
+private def ulift_rel {α: Type u} (rel: α -> α -> Prop) : ULift α -> ULift α -> Prop := fun a b => rel a.down b.down
+private def ulift_rel_hom {α: Type u} (rel: α -> α -> Prop) : ulift_rel rel ↪r rel where
+  toFun x := x.down
+  resp_rel := Iff.rfl
+  inj' := (Equiv.ulift _).inj
+
+def ulift : Ordinal.{u} -> Ordinal.{max u v} := by
+  refine lift (fun α relα _ => Ordinal.type' (ulift_rel relα) ?_) ?_
+  · exact (ulift_rel_hom relα).lift_wo
+  · intro α β relα relβ _ _ h
+    dsimp
+    apply sound
+    exact {
+      Equiv.congrULift h.toEquiv with
+      resp_rel := h.resp_rel
+    }
+
+instance : LE Ordinal where
+  le := by
+    refine lift₂ (fun _ _ relα relβ _ _ => Nonempty (relα ≼i relβ)) ?_
+    intro A B C D relA relB relC relD _ _ _ _ ac bd
+    dsimp
+    ext
+    apply Iff.intro
+    intro ⟨h⟩; refine ⟨?_⟩
+    apply h.congr
+    assumption
+    assumption
+    intro ⟨h⟩; refine ⟨?_⟩
+    apply h.congr
+    symm; assumption
+    symm; assumption
+
+instance : LT Ordinal where
+  lt := by
+    refine lift₂ (fun _ _ relα relβ _ _ => Nonempty (relα ≺i relβ)) ?_
+    intro A B C D relA relB relC relD _ _ _ _ ac bd
+    dsimp
+    ext
+    apply Iff.intro
+    intro ⟨h⟩; refine ⟨?_⟩
+    apply h.congr
+    assumption
+    assumption
+    intro ⟨h⟩; refine ⟨?_⟩
+    apply h.congr
+    symm; assumption
+    symm; assumption
+
+end Ordinal
