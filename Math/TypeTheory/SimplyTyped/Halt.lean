95a0cd23d6a79b5bd14d24a98e4abf8b60cccc3d
RustyYato <rustyyato@gmail.com>
2025-05-18 19:51:41 -0700

    name hered halts work only in the empty context

--- Math/TypeTheory/SimplyTyped/Halt.lean
+++ Math/TypeTheory/SimplyTyped/Halt.lean
@@ -4,14 +4,14 @@ namespace Term
 
 open SimpleLamType (TypeCtx)
 
-def HeredHalts {ctx: TypeCtx} {ty: SimpleLamType} (term: Term) (wt: term.IsSimplyWellTyped ctx ty) : Prop :=
+def HeredHalts {ty: SimpleLamType} (term: Term) (wt: term.IsSimplyWellTyped [] ty) : Prop :=
   match ty with
   | .void => False
   -- a function hereditarily halts iff it halts and for any halting input
   -- the function application also halts
   | .func arg_ty ret_ty =>
     term.Halts ∧ (
-      ∀{arg: Term} (ha: arg.IsSimplyWellTyped ctx arg_ty),
+      ∀{arg: Term} (ha: arg.IsSimplyWellTyped [] arg_ty),
         HeredHalts arg ha ->
         HeredHalts (app term arg) (by
           apply IsSimplyWellTyped.app
@@ -21,8 +21,13 @@ def HeredHalts {ctx: TypeCtx} {ty: SimpleLamType} (term: Term) (wt: term.IsSimpl
 
 namespace HeredHalts
 
-def reduce (r: Reduce term term') (wt: IsSimplyWellTyped ctx term ty) : HeredHalts term wt ↔ HeredHalts term' (wt.reduce r) := by
-  induction ty generalizing term term' ctx with
+def Halts {term: Term} {wt: term.IsSimplyWellTyped [] ty} (h: term.HeredHalts wt) : term.Halts := by
+  cases ty
+  contradiction
+  exact h.left
+
+def reduce (r: Reduce term term') (wt: IsSimplyWellTyped [] term ty) : HeredHalts term wt ↔ HeredHalts term' (wt.reduce r) := by
+  induction ty generalizing term term' with
   | void => simp [HeredHalts]
   | func arg_ty ret_ty iha ihr =>
     apply Iff.intro
@@ -43,7 +48,7 @@ def reduce (r: Reduce term term') (wt: IsSimplyWellTyped ctx term ty) : HeredHal
       apply h
       assumption
 
-def reduces_to (r: ReducesTo term term') (wt: IsSimplyWellTyped ctx term ty) : HeredHalts term wt ↔ HeredHalts term' (wt.reduces_to r) := by
+def reduces_to (r: ReducesTo term term') (wt: IsSimplyWellTyped [] term ty) : HeredHalts term wt ↔ HeredHalts term' (wt.reduces_to r) := by
   induction r with
   | refl => rfl
   | cons r rs ih =>
@@ -53,40 +58,12 @@ def reduces_to (r: ReducesTo term term') (wt: IsSimplyWellTyped ctx term ty) : H
 
 end HeredHalts
 
-def Reduce.hered_halts (r: Reduce term term') (wt: IsSimplyWellTyped ctx term ty) : HeredHalts term wt ↔ HeredHalts term' (wt.reduce r) := by
+def Reduce.hered_halts (r: Reduce term term') (wt: IsSimplyWellTyped [] term ty) : HeredHalts term wt ↔ HeredHalts term' (wt.reduce r) := by
   apply HeredHalts.reduce
   assumption
 
-def ReducesTo.hered_halts (r: ReducesTo term term') (wt: IsSimplyWellTyped ctx term ty) : HeredHalts term wt ↔ HeredHalts term' (wt.reduces_to r) := by
+def ReducesTo.hered_halts (r: ReducesTo term term') (wt: IsSimplyWellTyped [] term ty) : HeredHalts term wt ↔ HeredHalts term' (wt.reduces_to r) := by
   apply HeredHalts.reduces_to
   assumption
-
-def HeredHalts.weaken_at_level {level: ℕ} (term: Term) (new_ty: SimpleLamType)
-  (term_wt: term.IsSimplyWellTyped ctx ty):
-  term.HeredHalts term_wt ->
-  (term.weaken_at_level level).HeredHalts (term_wt.weaken_at_level level new_ty) := by
-  sorry
-
-def HeredHalts.weaken (term: Term) (new_ty: SimpleLamType)
-  (term_wt: term.IsSimplyWellTyped ctx ty):
-  term.HeredHalts term_wt ->
-  term.weaken.HeredHalts (term_wt.weaken new_ty) := by
-  apply weaken_at_level
-
-def HeredHalts.subst_at {var: ℕ} (term subst: Term)
-  (hn: var < ctx.length)
-  (term_wt: term.IsSimplyWellTyped ctx ty)
-  (subst_wt: subst.IsSimplyWellTyped (ctx.eraseIdx var) ctx[var]):
-  term.HeredHalts term_wt -> subst.HeredHalts subst_wt ->
-  (term.subst subst var).HeredHalts (term_wt.subst_at hn subst_wt) := by
-  sorry
-
-def HeredHalts.subst (term subst: Term)
-  (term_wt: term.IsSimplyWellTyped (arg_ty::ctx) ty)
-  (subst_wt: subst.IsSimplyWellTyped ctx arg_ty):
-  term.HeredHalts term_wt -> subst.HeredHalts subst_wt ->
-  (term.subst subst 0).HeredHalts (term_wt.subst subst_wt) := by
-  apply HeredHalts.subst_at (var := 0)
-  apply Nat.zero_lt_succ
 
 end Term
