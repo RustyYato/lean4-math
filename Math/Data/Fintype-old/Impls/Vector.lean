6e87619bf039d283fa016d0935a47785f259746a
RustyYato <rustyyato@gmail.com>
2025-05-26 21:03:12 -0700

    remove fintype old

--- Math/Data/Fintype-old/Impls/Vector.lean
+++
@@ -1,5 +0,0 @@
-import Math.Data.«Fintype-old».Impls.Pi
-import Math.Data.ListVector.Basic
-
-instance [Fintype α] : Fintype (List.Vector α n) :=
-  Fintype.ofEquiv (List.Vector.equivFinFunc _ _)
--- Math/Data/Fintype-old/Small.lean
+++
@@ -1,8 +0,0 @@
-import Math.Logic.Small.Basic
-import Math.Data.«Fintype-old».Defs
-
--- every fintype is always small in any universe
-instance [Fintype α] : Small.{u} α := by
-  classical
-  induction (Fintype.equivFin α) with | mk eqv =>
-  refine ⟨_, ⟨eqv.trans (Equiv.ulift _).symm⟩⟩
